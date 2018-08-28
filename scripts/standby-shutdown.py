f650d7aa88c5b54178621e36349524a6a43deaff
SebuZet <39659949+SebuZet@users.noreply.github.com>
2018-08-28 21:26:41 +0200

    Update standby-shutdown.py

--- scripts/standby-shutdown.py
+++ scripts/standby-shutdown.py
@@ -1,11 +1,11 @@
 #!/bin/python 
 # Simple script for requesting shutdown operation
 # After putting pin D5 to LOW and D6 to HIGH levels shutdown device will start power off procedure
-#
-# author: Sebastian Zaorski
+# 
+# author: Sebastian Zaorski 
 
-import RPi.GPIO as GPIO
-import os, sys, configparser
+import RPi.GPIO as GPIO  
+import configparser, sys, os
 
 # reads configuration file
 config = configparser.ConfigParser()
@@ -15,27 +15,25 @@ config.read_file(open(os.path.dirname(sys.argv[0]) + '/standby.ini'))
 if config.has_section('Communication'):
 
    if config.getboolean('Communication', 'Enabled'):
+      PinOE = config.getint('Communication', 'PinOE')
 
       if config.has_section('Shutdown'):
          PinLow = config.getint('Shutdown', 'PinLow')
          PinHigh = config.getint('Shutdown', 'PinHigh')
          print ('Requesting shut down with pins ' + str(PinLow) + ' (LOW) and ' + str(PinHigh) + ' (HIGH). Pin OE: ' + str(PinOE))
 
-         # Use the Broadcom SOC Pin numbers
-         GPIO.setmode(GPIO.BCM)
+         # Use the Broadcom SOC Pin numbers 
+         GPIO.setmode(GPIO.BCM)  
 
          # setup pins
          GPIO.setup(PinLow, GPIO.OUT)
          GPIO.output(PinLow, GPIO.LOW)
          GPIO.setup(PinHigh, GPIO.OUT)
          GPIO.output(PinHigh, GPIO.HIGH)
-
+         
          # enable communication (just in case)
-         if config.getboolean('Communication', 'ActivatePinOE'):
-            PinOE = config.getint('Communication', 'PinOE')
-            GPIO.setup(PinOE, GPIO.OUT)
-            GPIO.output(PinOE, GPIO.HIGH)
-
+         GPIO.setup(PinOE, GPIO.OUT)
+         GPIO.output(PinOE, GPIO.HIGH)
       else:
          print ('ERROR: \'Shutdown\' section doesn\'t exist in configuration file.')
 
